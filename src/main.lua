a9867ee32880a0e91c2a84b80bf159d0cfbc96dc
Ling Zhi Du <shdulingzhi@corp.netease.com>
2019-06-24 09:16:48 +0800

    fix: manual require lib

--- src/main.lua
+++ src/main.lua
@@ -1,22 +1,25 @@
+require("lib")
 
 local function main()
-
     print("Welcome to WarOfPet!!!")
 
     Native.FogMaskEnable(false)
     Native.FogEnable(false)
 
-    require('eventids')
+    require("eventids")
 
-    require('wage')
-    require('seller')
-    require('fight')    
+    require("wage")
+    require("seller")
+    require("fight")
 
-    Timer:create():start(0.01, true, function()
-        Native.SetCameraField(CameraField.TargetDistance, 2500.0, 0)
-        Native.SetCameraField(CameraField.AngleOfAttack, -50.0, 0)
-    end)
-    
+    Timer:create():start(
+        0.01,
+        true,
+        function()
+            Native.SetCameraField(CameraField.TargetDistance, 2500.0, 0)
+            Native.SetCameraField(CameraField.AngleOfAttack, -50.0, 0)
+        end
+    )
 end
 
 Timer:after(2, main)
